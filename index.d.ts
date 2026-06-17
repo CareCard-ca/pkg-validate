5f1a6a2171eb690cdbd04b2866b42e244d5520cd
Pankaj Singh <pankajpriscilla@gmail.com>
2026-06-16 22:52:42 -0400

    Add CareCard ID validation

--- index.d.ts
+++ index.d.ts
@@ -127,6 +127,8 @@ export const isValidJsonString: BoolValidator;
 export const isValidIntegerString: BoolValidator;
 /** Checks if the string is a valid UUID. */
 export const isValidUuidString: BoolValidator;
+/** Checks if the string is a valid lowercase 8-character CareCard ID. */
+export const isCcIdString: BoolValidator;
 /** Checks if the string contains only alphanumeric characters, spaces, underscores, or hyphens. */
 export const isCharactersString: BoolValidator;
 /** Checks if the string is a valid street address format. */
@@ -198,6 +200,7 @@ export const validate: {
     isValidJsonString: typeof isValidJsonString;
     isValidIntegerString: typeof isValidIntegerString;
     isValidUuidString: typeof isValidUuidString;
+    isCcIdString: typeof isCcIdString;
     isCharactersString: typeof isCharactersString;
     isStreetString: typeof isStreetString;
     isNameString: typeof isNameString;
